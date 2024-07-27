bf0fdea6429baf68c1c25c75db7996cfd6635740
Tobi-Daniel <oloyebmv@gmail.com>
2024-07-27 14:08:56 +0100

    scrape amazon product

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { scrapeAndStoreProduct } from '@/lib/actions';
 import { FormEvent, useState } from 'react'
 
 const isValidAmazonProductUrl = (url: string) => {
@@ -23,7 +24,7 @@ const SearchBar = () => {
     const [searchPrompt, setSearchPrompt] = useState('')
     const [isLoading, setIsLoading] = useState(false)
 
-    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault()
 
         const isValidLink = isValidAmazonProductUrl(searchPrompt)
@@ -34,6 +35,7 @@ const SearchBar = () => {
             setIsLoading(true)
 
             // Scrape the product page
+            const product = await scrapeAndStoreProduct(searchPrompt)
         } catch (error) {
             console.log(error);
 
